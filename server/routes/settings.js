d9ac9252a4832e4c4403e99263faa947bf47f447
Ubuntu <ubuntu@ip-172-31-30-250.ap-southeast-1.compute.internal>
2016-01-30 02:57:40 +0000

    changed apikey

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   "gcm":{
-		"apiKey":"AIzaSyADLJ2Ukop8TNNMfk_tqcCYgUi_Goo5xNo"
+		"apiKey":"AIzaSyBEe2CUs95A6avqgIui7iSBVDZd9E4b1Pc"
 	},
   "db":{
 		"mongo":{ "host":"localhost", "port":"27107", "name":"Alerto_1", "username":"mongoadmin", "password":"alertoadmin"}
